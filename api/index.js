0650dff210a44349e3d4b83bac82596c91c94587
Tiago Navarro <navarrotiago194@gmail.com>
2024-05-13 11:14:40 -0300

    update origin

--- api/index.js
+++ api/index.js
@@ -10,8 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "https://darwoft-blog.onrender.com",
+    origin: ["https://darwoft-blog.onrender.com", "http://localhost:5173"],
   })
 );
 
